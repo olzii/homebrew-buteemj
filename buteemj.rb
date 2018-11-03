6856da03024c2b8c5b634cdd93761f1410a33899
olzii <olzii@starsoft.mn>
2018-11-03 21:00:44 +0800

    ;;;;

--- buteemj.rb
+++ buteemj.rb
@@ -17,7 +17,7 @@ class Buteemj < Formula
     #                      "--prefix=#{prefix}"
     # system "cmake", ".", *std_cmake_args
     #system "make", "install" # if this fails, try separate make/make install steps
-	  bin.install "."
+	  bin.install "*"
 	#  system "install"
   end
 
