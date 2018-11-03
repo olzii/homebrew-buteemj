0285cf8cde842371b5abcd69a9e57dc6fbcac94a
olzii <olzii@Os-MacBook-Air.local>
2018-11-03 18:40:51 +0800

    add formula

---
+++ buteemj.rb
@@ -0,0 +1,35 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://www.rubydoc.info/github/Homebrew/brew/master/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Buteemj < Formula
+  desc "buteemj app"
+  homepage ""
+  url "https://github.com/olzii/buteemj/archive/v1.0.0.tar.gz"
+  sha256 "02c7d4c20fc4c1377cd6cb4d124fe31e3ae00e3161a77b282eccbcfaf25453d8"
+  # depends_on "cmake" => :build
+
+  def install
+    	# ENV.deparallelize  # if your formula fails when building in parallel
+    	# Remove unrecognized options if warned by configure
+    #system "./configure", "--disable-debug",
+    #                      "--disable-dependency-tracking",
+    #                      "--disable-silent-rules",
+    #                      "--prefix=#{prefix}"
+    # system "cmake", ".", *std_cmake_args
+    #system "make", "install" # if this fails, try separate make/make install steps
+	  bin.install "buteemj"
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test buteemj`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    # system "false"
+  end
+end
