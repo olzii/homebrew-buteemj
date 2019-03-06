bc2d07b54e8e32e15fd5043ceffafc1200b6e6e1
olzii <olzii@starsoft.mn>
2019-03-06 13:16:57 +0800

    sss

--- buteemj.rb
+++ buteemj.rb
@@ -34,7 +34,7 @@ class Buteemj < Formula
 	  bin.install "main"
 	  bin.install "buteemj-macos"
 
-	  system "chmod ugo+w #{bin}/snapshot.dat"
+	  system "chmod u+w #{bin}/snapshot.dat"
 	  system "./install.sh", "--arg1", "--prefix=#{prefix}"
 	  # chmod ugo+w snapshot.dat
 
